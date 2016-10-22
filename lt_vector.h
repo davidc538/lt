9c96300c6fd7d94d1d8341eee2faf8ce8cbb53c8
David Carpenter <davidc538@hotmail.com>
2016-10-22 13:11:23 -0400

    small changes

--- lt_vector.h
+++ lt_vector.h
@@ -16,7 +16,7 @@ void lt_vector_free(lt_vector* da);
 void lt_vector_delete(lt_vector* da);
 
 void lt_vector_each(lt_vector* da, void (func)(void*, void*), void* data);
-void lt_vector_expand(lt_vector* da, size_t new_cap);
+void lt_vector_expand(lt_vector* da, size_t factor);
 void lt_vector_insert(lt_vector* da, void* data);
 void* lt_vector_random_access(lt_vector* da, size_t index);
 void* lt_vector_binary_search(lt_vector* da, int (comparator)(void*, void*), void* to_find);
